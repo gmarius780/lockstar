1c00b8e2a3b77c406af80341fd0cee06a469f11c
Marius Gaechter <gmarius@ethz.ch>
2022-10-07 18:41:34 +0200

    AWGModule passed basic tests and works

--- code/client/python/lockstar_client/AWGClient.py
+++ code/client/python/lockstar_client/AWGClient.py
@@ -87,29 +87,29 @@ if __name__ == "__main__":
     if client.register_client_id():
         logging.info(f'Successfully initialized AWG module')
 
-        sampling_rate = 1000000
+        sampling_rate = 5000
         
-        # ch_one_chunks = [999, 1999, 2999, 3999, 4999]
-        # ch_two_chunks = [1999, 2999]
-        # # ch_one_buffer = np.concatenate((np.sin(np.linspace(0, 2*np.pi, num=1000)),
-        # #                                 np.linspace(0, 1, num=1000),
-        # #                                 np.cos(np.linspace(0, 6*np.pi, num=1000)),
-        # #                                 np.linspace(1, -3, num=1000),
-        # #                                 np.linspace(-3, 0, num=1000))).tolist()
+        ch_one_chunks = [999, 1999, 2999, 3999, 4999]
+        ch_two_chunks = [1999, 2999]
+        ch_one_buffer = np.concatenate((np.sin(np.linspace(0, 2*np.pi, num=1000)),
+                                        np.linspace(0, 1, num=1000),
+                                        np.cos(np.linspace(0, 6*np.pi, num=1000)),
+                                        np.linspace(1, -3, num=1000),
+                                        np.linspace(-3, 0, num=1000))).tolist()
         # ch_one_buffer = np.concatenate((np.sin(np.linspace(0, 50*2*np.pi, num=1000)),
         #                                 np.sin(np.linspace(0, 50*2*np.pi, num=1000)),
         #                                 np.sin(np.linspace(0, 50*2*np.pi, num=1000)),
         #                                 np.sin(np.linspace(0, 50*2*np.pi, num=1000)),
         #                                 np.sin(np.linspace(0, 50*2*np.pi, num=1000)))).tolist()
-        # ch_two_buffer = np.concatenate((np.cos(np.linspace(0, 50*4*np.pi, num=2000)),
-        #                                 np.linspace(1, 4, num=500), np.linspace(4, 1, num=500))).tolist()
+        ch_two_buffer = np.concatenate((np.cos(np.linspace(0, 50*4*np.pi, num=2000)),
+                                        np.linspace(1, 4, num=500), np.linspace(4, 1, num=500))).tolist()
         
 
-        ch_one_chunks = [1999]
-        ch_two_chunks = [1999]
+        # ch_one_chunks = [1999]
+        # ch_two_chunks = [1999]
         
-        ch_one_buffer = np.sin(np.linspace(0, 50*2*np.pi, num=2000)).tolist()
-        ch_two_buffer = np.sin(np.linspace(0, 50*2*np.pi, num=2000)).tolist()
+        # ch_one_buffer = np.sin(np.linspace(0, 50*2*np.pi, num=2000)).tolist()
+        # ch_two_buffer = np.sin(np.linspace(0, 50*2*np.pi, num=2000)).tolist()
 
 
         # print(client.set_ch_one_output_limits(0, 1))
