42d14fad792bfe32a1c56b7d45947646e8c68d7e
Marius Gaechter <gmarius@ethz.ch>
2022-12-08 08:51:32 +0100

    launch_from_config - bufferbasemodule

--- code/rpi/python/lockstar_rpi/Modules/IOModule_.py
+++ code/rpi/python/lockstar_rpi/Modules/IOModule_.py
@@ -262,7 +262,7 @@ class IOModule_(Module):
                                                     float(config_dict['out_range_ch_one_min']), 
                                                     float(config_dict['out_range_ch_one_max']), None, respond=False)
             
-            if 'linearization_one_enabled' in config_dict.keys():
+            if success and 'linearization_one_enabled' in config_dict.keys():
                 success = await self.enable_linearization_one(None, respond=False)
         except Exception as ex_one:
             logging.error(f'IOModule: could not set linearization_one from config: {ex_one}')
@@ -278,7 +278,7 @@ class IOModule_(Module):
                                                     float(config_dict['out_range_ch_two_min']), 
                                                     float(config_dict['out_range_ch_two_max']), None, respond=False)
             
-            if 'linearization_two_enabled' in config_dict.keys():
+            if success and 'linearization_two_enabled' in config_dict.keys():
                 success = await self.enable_linearization_two(None, respond=False)
         except Exception as ex:
             logging.error(f'IOModule: could not set linearization_two from config: {ex}')
