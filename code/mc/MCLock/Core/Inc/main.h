20aa5279af16860b676976b05a61ed214073f617
Marius Gaechter <gmarius@ethz.ch>
2022-12-27 16:52:42 +0100

    debug

--- code/mc/MCLock/Core/Inc/main.h
+++ code/mc/MCLock/Core/Inc/main.h
@@ -127,7 +127,8 @@ void start(void);
 #define Continuous_Lock 			0x0109
 
 //DEFINE WHICH MODULE TO RUN!!
-#define AWG_MODULE
+//#define AWG_MODULE
+#define SINGLE_PID_MODULE
 //#define LINEARIZATION_MODULE
 
 /* USER CODE END Private defines */
