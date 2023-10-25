14a616e072367bba4be0ca468ad6a267acfa911d
emustafa96 <emustafa@student.ethz.ch>
2023-10-25 10:44:10 +0200

    [mc - basictimer] Update prescaler for new APB clock

--- code/mc/dev/Core/HAL/BasicTimer.hpp
+++ code/mc/dev/Core/HAL/BasicTimer.hpp
@@ -19,7 +19,7 @@
  * */
 class BasicTimer {
 public:
-	static const uint32_t INTERNAL_CLOCK_FREQUENCY = 90e6;
+	static const uint32_t INTERNAL_CLOCK_FREQUENCY = 275e6;
 
 	BasicTimer(uint8_t timer_x, uint32_t auto_reload, uint32_t prescaler);
 	void set_auto_reload(uint32_t value); //specifies after how many counts at base_frequency/prescaler the counter should be reset --> interrupt is triggered
