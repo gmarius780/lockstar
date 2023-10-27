6443cafac183aed30140dbcc6ef00505f37cc2b3
emustafa96 <emustafa@student.ethz.ch>
2023-10-27 17:22:24 +0200

    [mc - DACDev] Add BDMA configuration

--- code/mc/dev/Core/Modules/dac_config.h
+++ code/mc/dev/Core/Modules/dac_config.h
@@ -13,8 +13,11 @@ extern "C" {
 #define DAC2_CLEAR_PORT CLR5_GPIO_Port
 #define DAC2_CLEAR_PIN CLR5_Pin
 
+#define DAC1_CLEAR_PORT CLR6_GPIO_Port
+#define DAC1_CLEAR_PIN CLR6_Pin
 
-#define USE_SPI6
+
+#define USE_SPI5
 
 
 #define SAMPLES 1100
@@ -23,7 +26,7 @@ extern "C" {
   #define DAC2_SPI SPI2
   #define DAC2_DMA DMA1
   #define DAC2_DMA_STREAM LL_DMA_STREAM_3
-  #define DMAMUX1_REQ_DAC_SPI_TX LL_DMAMUX1_REQ_SPI2_TX
+  #define DMAMUX_REQ_DAC_SPI_TX LL_DMAMUX1_REQ_SPI2_TX
   #define DAC2_TX_DMA_STREAM DMA1_Stream3
   #define DAC2_SYNC_PORT SPI2_NSS_GPIO_Port
   #define DAC2_SYNC_PIN SPI2_NSS_Pin
@@ -34,7 +37,7 @@ extern "C" {
   #define DAC2_SPI SPI5
   #define DAC2_DMA DMA2
   #define DAC2_DMA_STREAM LL_DMA_STREAM_3
-  #define DMAMUX1_REQ_DAC_SPI_TX LL_DMAMUX1_REQ_SPI5_TX
+  #define DMAMUX_REQ_DAC_SPI_TX LL_DMAMUX1_REQ_SPI5_TX
   #define DAC2_TX_DMA_STREAM DMA2_Stream3
   #define DAC2_SYNC_PORT DAC_2_Sync_GPIO_Port
   #define DAC2_SYNC_PIN DAC_2_Sync_Pin
@@ -42,14 +45,15 @@ extern "C" {
 #endif
 
 #ifdef USE_SPI6
+  #define IS_BDMA
   #define DAC2_SPI SPI6
   #define DAC2_DMA BDMA
-  #define DAC2_DMA_STREAM LL_DMA_STREAM_3
-  #define DMAMUX1_REQ_DAC_SPI_TX LL_DMAMUX1_REQ_SPI5_TX
-  #define DAC2_TX_DMA_STREAM DMA2_Stream3
-  #define DAC2_SYNC_PORT DAC_2_Sync_GPIO_Port
-  #define DAC2_SYNC_PIN DAC_2_Sync_Pin
-  #define TX_DMA_ClearFlag LL_DMA_ClearFlag_TC3
+  #define DAC2_DMA_STREAM LL_BDMA_CHANNEL_1
+  #define DMAMUX_REQ_DAC_SPI_TX LL_DMAMUX2_REQ_SPI6_TX
+  #define DAC2_TX_DMA_STREAM BDMA_Channel1
+  #define DAC2_SYNC_PORT DAC_1_Sync_GPIO_Port
+  #define DAC2_SYNC_PIN DAC_1_Sync_Pin
+  #define TX_DMA_ClearFlag LL_BDMA_ClearFlag_TC1
 #endif
 
 
