7454ce4155552ec33beb502b0a5b102ac957d555
emustafa96 <emustafa@student.ethz.ch>
2023-11-09 16:11:49 +0100

    [mc - AITest] Add cycle counter

--- code/mc/dev/Core/Modules/AITestModule.cpp
+++ code/mc/dev/Core/Modules/AITestModule.cpp
@@ -9,12 +9,17 @@
 #include "stm32h725xx.h"
 #include "stm32h7xx_it.h"
 #include "../HAL/ADCDevice.hpp"
+#include "../HAL/DACDevice.hpp"
 #include "adc_config.h"
+#include "dac_config.h"
 #include "../HAL/leds.hpp"
 #include <stdio.h>
 
 #ifdef AI_TEST_MODULE
-
+extern ADC_HandleTypeDef hadc3;
+volatile uint32_t start_counter = 0;
+volatile uint32_t end_counter = 0;
+volatile uint32_t diff = 0;
 class AITestModule
 {
 public:
@@ -25,16 +30,33 @@ public:
 	void run()
 	{
 		ADC_Dev = new ADC_Device(&ADC_conf, ADC_BIPOLAR_10V, ADC_BIPOLAR_10V);
-
+		dac_1 = new DAC1_Device(&DAC1_conf);
+		dac_2 = new DAC2_Device(&DAC2_conf);
+		dac_1->config_output();
+		dac_2->config_output();
 		turn_LED2_on();
 		turn_LED3_on();
-
 		// ADC_Dev->start_conversion();
+		SET_BIT(DWT->CTRL, DWT_CTRL_CYCCNTENA_Msk);
+		start_counter = DWT->CYCCNT;
+		ADC_Dev->start_conversion();
+		m1 = ADC_Dev->channel1->get_result();
+		m2 = ADC_Dev->channel2->get_result();
+		dac_1->write(m1);
+		dac_2->write(m1);
+		end_counter = DWT->CYCCNT;
+
+		diff = end_counter - start_counter;
+		__asm__ __volatile__("" ::"m"(diff));
 		while (true)
 		{
-			ADC_Dev->start_conversion();
-			m1 = ADC_Dev->channel1->get_result();
-			m2 = ADC_Dev->channel2->get_result();
+			// start = CYCCNT;
+			// ADC_Dev->start_conversion();
+			// m1 = ADC_Dev->channel1->get_result();
+			// m2 = ADC_Dev->channel2->get_result();
+			// dac_1->write(m1);
+			// dac_2->write(m1);
+			// end = CYCCNT;
 			// printf("Channel 1: %f\n", m1);
 			// printf("Channel 2: %f\n", m2);
 		}
@@ -42,6 +64,8 @@ public:
 
 public:
 	ADC_Device *ADC_Dev;
+	DAC1_Device *dac_1;
+	DAC2_Device *dac_2;
 	float m1 = 0;
 	float m2 = 0;
 };
@@ -51,17 +75,32 @@ AITestModule *module;
 /******************************
  *         INTERRUPTS          *
  *******************************/
-__attribute__((section("sram_func")))
-void DMA1_Stream2_IRQHandler(void)
+/********************
+||      DAC1      ||
+********************/
+void BDMA_Channel1_IRQHandler(void)
 {
-	module->ADC_Dev->dma_receive_callback();
-}
-__attribute__((section("sram_func")))
-void DMA1_Stream3_IRQHandler(void)
-{
-	module->ADC_Dev->dma_transmission_callback();
+	module->dac_1->dma_transmission_callback();
 }
 
+void SPI6_IRQHandler(void)
+{
+	module->dac_1->dma_transmission_callback();
+}
+/********************
+||      DAC2      ||
+********************/
+void DMA2_Stream3_IRQHandler(void)
+{
+	module->dac_2->dma_transmission_callback();
+}
+void SPI5_IRQHandler(void)
+{
+	module->dac_2->dma_transmission_callback();
+}
+/********************
+||       ADC       ||
+********************/
 void DMA1_Stream4_IRQHandler(void)
 {
 	module->ADC_Dev->dma_receive_callback();
