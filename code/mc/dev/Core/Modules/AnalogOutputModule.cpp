caafef7966aae1df570e07da7a9d72f65cef9cda
emustafa96 <emustafa@student.ethz.ch>
2023-11-22 16:19:06 +0100

    [mc - AOM] Update IRQs

--- code/mc/dev/Core/Modules/AnalogOutputModule.cpp
+++ code/mc/dev/Core/Modules/AnalogOutputModule.cpp
@@ -7,7 +7,6 @@
 #include "main.h"
 #include "stm32h725xx.h"
 #include "stm32h7xx_hal.h"
-#include "stm32h7xx_it.h"
 #include "stm32h7xx_hal_gpio.h"
 
 #include "../HAL/ADCDevice.hpp"
@@ -23,11 +22,11 @@
 
 #ifdef ANALOG_OUTPUT_MODULE
 
-
-
-class AnalogOutputModule: public ScopeModule {
+class AnalogOutputModule : public ScopeModule
+{
 public:
-	AnalogOutputModule() : ScopeModule() {
+	AnalogOutputModule() : ScopeModule()
+	{
 		initialize_rpi();
 		turn_LED6_off();
 		turn_LED4_on();
@@ -35,24 +34,29 @@ public:
 		output_value_two = 5;
 	}
 
-	void run() {
+	void run()
+	{
 		initialize_adc_dac(ADC_UNIPOLAR_10V, ADC_UNIPOLAR_10V);
 		this->dac_1->write(0);
 		this->dac_2->write(0);
 
 		/*** work loop ***/
-		while(true) {
+		while (true)
+		{
 			// HAL_Delay(50);
 		}
 	}
 
-	void handle_rpi_input() {
-		if (ScopeModule::handle_rpi_base_methods() == false) { //if base class doesn't know the called method
+	void handle_rpi_input()
+	{
+		if (ScopeModule::handle_rpi_base_methods() == false)
+		{ // if base class doesn't know the called method
 			/*** Package format: method_identifier (uint32_t) | method specific arguments (defined in the methods directly) ***/
-			RPIDataPackage* read_package = rpi->get_read_package();
+			RPIDataPackage *read_package = rpi->get_read_package();
 
 			// switch between method_identifier
-			switch (read_package->pop_from_buffer<uint32_t>()) {
+			switch (read_package->pop_from_buffer<uint32_t>())
+			{
 			case METHOD_OUTPUT_ON:
 				output_on(read_package);
 				break;
@@ -70,18 +74,18 @@ public:
 				break;
 			default:
 				/*** send NACK because the method_identifier is not valid ***/
-				RPIDataPackage* write_package = rpi->get_write_package();
+				RPIDataPackage *write_package = rpi->get_write_package();
 				write_package->push_nack();
 				rpi->send_package(write_package);
 				break;
 			}
 		}
-
 	}
 
 	/*** START: METHODS ACCESSIBLE FROM THE RPI ***/
 	static const uint32_t METHOD_OUTPUT_ON = 11;
-	void output_on(RPIDataPackage* read_package) {
+	void output_on(RPIDataPackage *read_package)
+	{
 		this->is_output_on = true;
 		this->is_output_ttl = false;
 		this->dac_1->write(this->output_value_one);
@@ -89,13 +93,14 @@ public:
 		turn_LED6_on();
 
 		/*** send ACK ***/
-		RPIDataPackage* write_package = rpi->get_write_package();
+		RPIDataPackage *write_package = rpi->get_write_package();
 		write_package->push_ack();
 		rpi->send_package(write_package);
 	}
 
 	static const uint32_t METHOD_OUTPUT_OFF = 12;
-	void output_off(RPIDataPackage* read_package) {
+	void output_off(RPIDataPackage *read_package)
+	{
 		this->is_output_on = false;
 		this->is_output_ttl = false;
 		this->dac_1->write(0);
@@ -103,72 +108,80 @@ public:
 		turn_LED6_off();
 
 		/*** send ACK ***/
-		RPIDataPackage* write_package = rpi->get_write_package();
+		RPIDataPackage *write_package = rpi->get_write_package();
 		write_package->push_ack();
 		rpi->send_package(write_package);
 	}
 
 	static const uint32_t METHOD_OUTPUT_TTL = 13;
-	void output_ttl(RPIDataPackage* read_package) {
+	void output_ttl(RPIDataPackage *read_package)
+	{
 		this->is_output_on = false;
 		this->is_output_ttl = true;
 		turn_LED6_off();
 
 		/*** send ACK ***/
-		RPIDataPackage* write_package = rpi->get_write_package();
+		RPIDataPackage *write_package = rpi->get_write_package();
 		write_package->push_ack();
 		rpi->send_package(write_package);
 	}
 
 	static const uint32_t METHOD_SET_CH_ONE_OUTPUT = 16;
-	void set_ch_one_output(RPIDataPackage* read_package) {
+	void set_ch_one_output(RPIDataPackage *read_package)
+	{
 		/***Read arguments***/
 		output_value_one = read_package->pop_from_buffer<float>();
 		if (this->is_output_on)
 			this->dac_1->write(output_value_one);
 
 		/*** send ACK ***/
-		RPIDataPackage* write_package = rpi->get_write_package();
+		RPIDataPackage *write_package = rpi->get_write_package();
 		write_package->push_ack();
 		rpi->send_package(write_package);
 	}
 
 	static const uint32_t METHOD_SET_CH_TWO_OUTPUT = 17;
-	void set_ch_two_output(RPIDataPackage* read_package) {
+	void set_ch_two_output(RPIDataPackage *read_package)
+	{
 		/***Read arguments***/
 		output_value_two = read_package->pop_from_buffer<float>();
 		if (this->is_output_on)
 			this->dac_2->write(output_value_two);
 
 		/*** send ACK ***/
-		RPIDataPackage* write_package = rpi->get_write_package();
+		RPIDataPackage *write_package = rpi->get_write_package();
 		write_package->push_ack();
 		rpi->send_package(write_package);
 	}
 
 	/*** END: METHODS ACCESSIBLE FROM THE RPI ***/
 
-	void rpi_dma_in_interrupt() {
+	void rpi_dma_in_interrupt()
+	{
 
-		if(rpi->dma_in_interrupt())
+		if (rpi->dma_in_interrupt())
 		{ /*got new package from rpi*/
 			handle_rpi_input();
-		} else
+		}
+		else
 		{ /* error */
-
 		}
 	}
 
-	void digital_in_rising_edge() {
-		if (this->is_output_ttl) {
+	void digital_in_rising_edge()
+	{
+		if (this->is_output_ttl)
+		{
 			this->dac_1->write(this->output_value_one);
 			this->dac_2->write(this->output_value_two);
 			turn_LED6_on();
 		}
 	}
 
-	void digital_in_falling_edge() {
-		if (this->is_output_ttl) {
+	void digital_in_falling_edge()
+	{
+		if (this->is_output_ttl)
+		{
 			this->dac_1->write(0);
 			this->dac_2->write(0);
 			turn_LED6_off();
@@ -180,7 +193,6 @@ public:
 	float output_value_one, output_value_two;
 };
 
-
 AnalogOutputModule *module;
 
 /******************************
@@ -188,15 +200,16 @@ AnalogOutputModule *module;
  ******************************
  * Callbacks are functions that are executed in response to events such as SPI communication finished, change on trigger line etc */
 
-void HAL_GPIO_EXTI_Callback (uint16_t gpio_pin)
+void EXTI9_5_IRQHandler(uint16_t gpio_pin)
 {
-	if(gpio_pin == DigitalIn_Pin) {
+	if (gpio_pin == DigitalIn_Pin)
+	{
 		// Rising Edge
-		if(HAL_GPIO_ReadPin(DigitalIn_GPIO_Port, DigitalIn_Pin) == GPIO_PIN_RESET)
+		if (HAL_GPIO_ReadPin(DigitalIn_GPIO_Port, DigitalIn_Pin) == GPIO_PIN_RESET)
 			module->digital_in_rising_edge();
 
 		// Falling Edge
-		if(HAL_GPIO_ReadPin(DigitalIn_GPIO_Port, DigitalIn_Pin) == GPIO_PIN_SET)
+		if (HAL_GPIO_ReadPin(DigitalIn_GPIO_Port, DigitalIn_Pin) == GPIO_PIN_SET)
 			module->digital_in_falling_edge();
 	}
 
@@ -204,7 +217,6 @@ void HAL_GPIO_EXTI_Callback (uint16_t gpio_pin)
 }
 
 // DMA Interrupts. You probably don't want to change these, they are neccessary for the low-level communications between MCU, converters and RPi
-
 
 /********************
 ||      DAC1      ||
@@ -252,18 +264,21 @@ void DMA1_Stream1_IRQHandler(void)
 {
 	module->rpi->dma_out_interrupt();
 }
-void SPI1_IRQHandler(void) {
+void SPI1_IRQHandler(void)
+{
 	module->rpi->spi_interrupt();
 }
 
-void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+void TIM4_IRQHandler(void)
 {
-	if (htim->Instance == TIM4) {
-		module->rpi->comm_reset_timer_interrupt();
-	}
-	else if(htim->Instance == TIM7) {
-		module->scope_timer_interrupt();
-	}
+
+	module->rpi->comm_reset_timer_interrupt();
+}
+
+void TIM7_IRQHandler(void)
+{
+
+	module->scope_timer_interrupt();
 }
 
 /******************************
@@ -278,10 +293,11 @@ void start(void)
 	extern unsigned __sram_func_start, __sram_func_end, __sram_func_loadaddr;
 	volatile unsigned *src = &__sram_func_loadaddr;
 	volatile unsigned *dest = &__sram_func_start;
-	while (dest < &__sram_func_end) {
-	  *dest = *src;
-	  src++;
-	  dest++;
+	while (dest < &__sram_func_end)
+	{
+		*dest = *src;
+		src++;
+		dest++;
 	}
 
 	/* After power on, give all devices a moment to properly start up */
@@ -290,7 +306,6 @@ void start(void)
 	module = new AnalogOutputModule();
 
 	module->run();
-
 }
 
 #endif
