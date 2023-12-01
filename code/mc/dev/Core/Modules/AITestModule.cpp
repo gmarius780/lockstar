d7d573aea99efedaa17fb1a746d3c95a5021f2d3
emustafa96 <emustafa@student.ethz.ch>
2023-12-01 14:35:11 +0100

    [mc - AITM] Clean up with IRQ region changes

--- code/mc/dev/Core/Modules/AITestModule.cpp
+++ code/mc/dev/Core/Modules/AITestModule.cpp
@@ -1,3 +1,4 @@
+#ifdef AI_TEST_MODULE
 /*
  * AITestModule.cpp
  *
@@ -10,14 +11,11 @@
 #include "stm32h7xx_it.h"
 #include "../HAL/ADCDevice.hpp"
 #include "../HAL/DACDevice.hpp"
+#include "../HAL/BasicTimer.hpp"
 #include "adc_config.h"
 #include "dac_config.h"
 #include "../HAL/leds.hpp"
-#include <stdio.h>
 
-#ifdef AI_TEST_MODULE
-
-extern ADC_HandleTypeDef hadc3;
 uint32_t start_cycle, end_cycle, total_cycles;
 class AITestModule
 {
@@ -35,28 +33,27 @@ public:
 		dac_2->config_output();
 		turn_LED2_on();
 		turn_LED3_on();
-		enable_cycle_counter();
 
+		prescaler = 275;
+		counter_max = 1000;
+		this->sampling_timer = new BasicTimer(2, counter_max, prescaler);
 
-		// start_cycle = get_cycle_count();
+		dac_1->write(0);
+		dac_2->write(0);
+		sampling_timer->enable_interrupt();
+		sampling_timer->enable();
 
-		// ADC_Dev->start_conversion();
-		// m1 = ADC_Dev->channel1->get_result();
-		// m2 = ADC_Dev->channel2->get_result();
-		// dac_1->write(m1);
-		// dac_2->write(m1);
-		// end_cycle = get_cycle_count();
-		// total_cycles = end_cycle - start_cycle;
 		while (true)
 		{
-			ADC_Dev->start_conversion();
-			m1 = ADC_Dev->channel1->get_result();
-			m2 = ADC_Dev->channel2->get_result();
-			dac_1->write(m1);
-			dac_2->write(m1);
-			// printf("Channel 1: %f\n", m1);
-			// printf("Channel 2: %f\n", m2);
 		}
+	}
+	void sampling_timer_interrupt()
+	{
+		ADC_Dev->start_conversion();
+		m1 = ADC_Dev->channel1->get_result();
+		m2 = ADC_Dev->channel2->get_result();
+		dac_1->write(m1);
+		dac_2->write(m2);
 	}
 
 public:
@@ -65,6 +62,8 @@ public:
 	DAC2_Device *dac_2;
 	float m1 = 0;
 	float m2 = 0;
+	BasicTimer *sampling_timer;
+	uint32_t counter_max, prescaler;
 };
 
 AITestModule *module;
@@ -72,38 +71,47 @@ AITestModule *module;
 /******************************
  *         INTERRUPTS          *
  *******************************/
+
 /********************
 ||      DAC1      ||
 ********************/
-void BDMA_Channel1_IRQHandler(void)
+__attribute__((section(".itcmram"))) void BDMA_Channel1_IRQHandler(void)
 {
 	module->dac_1->dma_transmission_callback();
 }
-
-void SPI6_IRQHandler(void)
+__attribute__((section(".itcmram"))) void SPI6_IRQHandler(void)
 {
 	module->dac_1->dma_transmission_callback();
 }
 /********************
 ||      DAC2      ||
 ********************/
-void DMA2_Stream3_IRQHandler(void)
+__attribute__((section(".itcmram"))) void DMA2_Stream3_IRQHandler(void)
 {
 	module->dac_2->dma_transmission_callback();
 }
-void SPI5_IRQHandler(void)
+__attribute__((section(".itcmram"))) void SPI5_IRQHandler(void)
 {
 	module->dac_2->dma_transmission_callback();
+}
+
+/********************
+||      TIM2       ||
+********************/
+__attribute__((section(".itcmram"))) void TIM2_IRQHandler(void)
+{
+	LL_TIM_ClearFlag_UPDATE(TIM2);
+	module->sampling_timer_interrupt();
 }
 /********************
 ||       ADC       ||
 ********************/
-void DMA1_Stream4_IRQHandler(void)
+__attribute__((section(".itcmram"))) void DMA1_Stream4_IRQHandler(void)
 {
 	module->ADC_Dev->dma_receive_callback();
 }
 
-void DMA1_Stream5_IRQHandler(void)
+__attribute__((section(".itcmram"))) void DMA1_Stream5_IRQHandler(void)
 {
 	module->ADC_Dev->dma_transmission_callback();
 }
@@ -118,20 +126,6 @@ void DMA1_Stream5_IRQHandler(void)
  ******************************/
 void start(void)
 {
-	/* To speed up the access to functions, that are often called, we store them in the RAM instead of the FLASH memory.
-	 * RAM is volatile. We therefore need to load the code into RAM at startup time. For background and explanations,
-	 * check https://rhye.org/post/stm32-with-opencm3-4-memory-sections/
-	 * */
-	extern unsigned __sram_func_start, __sram_func_end, __sram_func_loadaddr;
-	volatile unsigned *src = &__sram_func_loadaddr;
-	volatile unsigned *dest = &__sram_func_start;
-	while (dest < &__sram_func_end)
-	{
-		*dest = *src;
-		src++;
-		dest++;
-	}
-
 	/* After power on, give all devices a moment to properly start up */
 	HAL_Delay(200);
 
