b89f06878120597973422980d9365be4192578b7
emustafa96 <emustafa@student.ethz.ch>
2023-12-05 10:39:29 +0100

    [mc - FGM] Add rpi

--- code/mc/dev/Core/Modules/FGModule.cpp
+++ code/mc/dev/Core/Modules/FGModule.cpp
@@ -9,16 +9,8 @@
 #include "../HAL/DACDevice.hpp"
 #include "../HAL/BasicTimer.hpp"
 #include "dac_config.h"
+#include "BufferBaseModule.h"
 
-/* Reference values in Q1.31 format */
-#define ANGLE_CORDIC (int32_t)0x10000000 /* pi/8 in CORDIC input angle mapping */
-#define ANGLE_LIB (int32_t)0x08000000    /* pi/8 in arm math lib input angle mapping */
-#define MODULUS (int32_t)0x7FFFFFFF      /* 1 */
-#define COS_REF (int32_t)0x7641AF3C      /* cos(pi/8) reference value */
-#define SIN_REF (int32_t)0x30FBC54D      /* sin(pi/8) reference value */
-#define ERROR (int32_t)0x00010000
-#define PASS 0
-#define FAIL 1
 
 #define MAX_VALUE 0xFFFFFFFF
 #define FIXED_POINT_FRACTIONAL_BITS 31
@@ -42,32 +34,32 @@ float *pCalculatedSin = aCalculatedSin;
 float *dacPointer = aCalculatedSin;
 float *endPointer = aCalculatedSin + (ARRAY_SIZE - 1);
 
-class FGModule
+class FGModule : public BufferBaseModule
 {
+    static const uint32_t BUFFER_LIMIT_kBYTES = 160; // if this is chosen to large (200) there is no warning, the MC simply crashes (hangs in syscalls.c _exit())
+    static const uint32_t MAX_NBR_OF_CHUNKS = 100;
+
 public:
     FGModule()
     {
-
+        initialize_rpi();
         LL_CORDIC_Config(CORDIC,
-                         LL_CORDIC_FUNCTION_ARCTANGENT,     /* cosine function */
-                         LL_CORDIC_PRECISION_6CYCLES, /* max precision for q1.31 cosine */
-                         LL_CORDIC_SCALE_6,           /* no scale */
-                         LL_CORDIC_NBWRITE_1,         /* One input data: angle. Second input data (modulus) is 1
-                                  after cordic reset */
-                         LL_CORDIC_NBREAD_1,          /* Two output data: cosine, then sine */
-                         LL_CORDIC_INSIZE_32BITS,     /* q1.31 format for input data */
+                         LL_CORDIC_FUNCTION_ARCTANGENT, /* cosine function */
+                         LL_CORDIC_PRECISION_6CYCLES,   /* max precision for q1.31 cosine */
+                         LL_CORDIC_SCALE_6,             /* no scale */
+                         LL_CORDIC_NBWRITE_1,           /* One input data: angle. Second input data (modulus) is 1
+                                    after cordic reset */
+                         LL_CORDIC_NBREAD_1,            /* Two output data: cosine, then sine */
+                         LL_CORDIC_INSIZE_32BITS,       /* q1.31 format for input data */
                          LL_CORDIC_OUTSIZE_32BITS);
     }
     void run()
     {
 
-        dac_1 = new DAC1_Device(&DAC1_conf);
-        dac_2 = new DAC2_Device(&DAC2_conf);
-        dac_1->config_output();
-        dac_2->config_output();
+        initialize_adc_dac(ADC_UNIPOLAR_10V, ADC_UNIPOLAR_10V);
 
-        prescaler = 275;
-        counter_max = 1000;
+        prescaler = 0;
+        counter_max = 549;
         this->sampling_timer = new BasicTimer(2, counter_max, prescaler);
 
         dac_1->write(0);
@@ -78,25 +70,63 @@ public:
         /* Write remaining angles and read sine results */
         sampling_timer->enable_interrupt();
         sampling_timer->enable();
+
+        // allocate buffer and chunk space
+        this->buffer = new float[BUFFER_LIMIT_kBYTES * 250]; // contains buffer_one and buffer_two sequentially
+        this->chunks = new uint32_t[MAX_NBR_OF_CHUNKS];      // contains chuncks_one and chunks_two sequentially
+
         for (uint32_t i = 1; i < ARRAY_SIZE; i++)
         {
             // start_angle += step_size;
             // CORDIC->WDATA = start_angle;
             CORDIC->WDATA = angle_values[i];
 
-            *pCalculatedSin++ = to_float(CORDIC->RDATA, 133.3*4, 4);
+            *pCalculatedSin++ = to_float(CORDIC->RDATA, 133.3 * 4, 4);
         }
         /* Read last result */
-        *pCalculatedSin = to_float(CORDIC->RDATA, 133.3*4, 4);
+        *pCalculatedSin = to_float(CORDIC->RDATA, 133.3 * 4, 4);
 
         while (true)
         {
         }
     }
 
+    void handle_rpi_input()
+    {
+        if (handle_rpi_base_methods() == false)
+        { // if base class doesn't know the called method
+            /*** Package format: method_identifier (uint32_t) | method specific arguments (defined in the methods directly) ***/
+            RPIDataPackage *read_package = this->rpi->get_read_package();
+            // switch between method_identifier
+            switch (read_package->pop_from_buffer<uint32_t>())
+            {
+            default:
+                /*** send NACK because the method_identifier is not valid ***/
+                RPIDataPackage *write_package = rpi->get_write_package();
+                write_package->push_nack();
+                rpi->send_package(write_package);
+                break;
+            }
+        }
+    }
+
+    /*** START: METHODS ACCESSIBLE FROM THE RPI ***/
+
+    /*** END: METHODS ACCESSIBLE FROM THE RPI ***/
+
+    void rpi_dma_in_interrupt()
+    {
+        if (rpi->dma_in_interrupt())
+        { /*got new package from rpi*/
+            handle_rpi_input();
+        }
+        else
+        { /* error */
+        }
+    }
+
     void sampling_timer_interrupt()
     {
-        // float value = ((float)*(dacPointer++) / (float)(1 << FIXED_POINT_FRACTIONAL_BITS)) * 5;
         if (dacPointer < endPointer)
         {
             this->dac_1->write(*dacPointer);
@@ -108,12 +138,6 @@ public:
             dacPointer = aCalculatedSin;
         }
     }
-
-public:
-    DAC_Device *dac_1;
-    DAC_Device *dac_2;
-    BasicTimer *sampling_timer;
-    uint32_t counter_max, prescaler;
 };
 
 FGModule *module;
@@ -148,11 +172,35 @@ __attribute__((section(".itcmram"))) void SPI5_IRQHandler(void)
 {
     module->dac_2->dma_transmission_callback();
 }
+/********************
+||       RPI       ||
+********************/
+__attribute__((section(".itcmram"))) void DMA1_Stream0_IRQHandler(void)
+{
+    module->rpi_dma_in_interrupt();
+}
+__attribute__((section(".itcmram"))) void DMA1_Stream1_IRQHandler(void)
+{
+    module->rpi->dma_out_interrupt();
+}
+__attribute__((section(".itcmram"))) void SPI1_IRQHandler(void)
+{
+    module->rpi->spi_interrupt();
+}
+/********************
+||      Timer      ||
+********************/
 __attribute__((section(".itcmram"))) void TIM2_IRQHandler(void)
 {
     LL_TIM_ClearFlag_UPDATE(TIM2);
     module->sampling_timer_interrupt();
 }
+__attribute__((section(".itcmram"))) void TIM4_IRQHandler(void)
+{
+    LL_TIM_ClearFlag_UPDATE(TIM4);
+    // module->rpi->comm_reset_timer_interrupt();
+}
+
 /******************************
  *       MAIN FUNCTION        *
  ******************************/
