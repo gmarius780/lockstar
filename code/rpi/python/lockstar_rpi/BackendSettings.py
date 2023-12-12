ff05e555173302022653e1e3a04af06eac219bf4
emustafa96 <emustafa@student.ethz.ch>
2023-12-12 15:12:58 +0000

    [py - backend] Increase SPI speed

--- code/rpi/python/lockstar_rpi/BackendSettings.py
+++ code/rpi/python/lockstar_rpi/BackendSettings.py
@@ -6,7 +6,7 @@ class BackendSettings:
     # backend_port = 10780
     read_buffer_limit_bytes = 50 * 10 ** 6
     current_module_config_file = './AOM.config'
-    mc_communication_speed_Hz = 1000000
+    mc_communication_speed_Hz = 25000000
     mc_gpio_input_channel = 8
     debug_mode = False
     mc_write_buffer_size = 4096
